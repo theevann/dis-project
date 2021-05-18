8ede3c1b8302d3f99a359c492b18cc849160abfe
Evann Courdier <evann.courdier@epfl.ch>
2021-05-19 00:16:32 +0200

    Delete build file supervisor

--- Initial_Material/controllers/supervisor/build/release/supervisor.d
+++
@@ -1,6 +0,0 @@
-build/release/supervisor.o: supervisor.c \
- C:\Users\Nicolas.DESKTOP-VQRDK4S\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
- C:\Users\Nicolas.DESKTOP-VQRDK4S\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
- C:\Users\Nicolas.DESKTOP-VQRDK4S\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
- C:\Users\Nicolas.DESKTOP-VQRDK4S\AppData\Local\Programs\Webots/include/controller/c/webots/receiver.h \
- C:\Users\Nicolas.DESKTOP-VQRDK4S\AppData\Local\Programs\Webots/include/controller/c/webots/supervisor.h
